d186d5ff6817249c8937eb51817e554ca642587a
ihjuan3 <jugopeis2001@gmail.com>
2024-02-24 20:02:28 -0800

    DONE

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     print(f'The total number of students \t \t {total:3d}')
     print(f'The number of males and females \t {male:3d} \t {female:3d}')
 
-    m_perc= (male/100)* 100
-    f_perc= (female/100)* 100
+    m_perc= (male/total)* 100
+    f_perc= (female/total)* 100
     print(f'The percentage of males and females \t {m_perc:.2f}% \t {f_perc:.2f}%')
     
     return m_perc, f_perc
